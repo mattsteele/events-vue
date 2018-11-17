c4416b6358cbaa49995b350fc021675af724eb86
Matt Steele <matt@matt-steele.com>
2018-11-17 12:46:21 -0700

    Add BaseIcon global component.

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,9 +1,10 @@
 <template>
     <router-link class="event-link" :to="{ name: 'event-show', params: { id: '1' }}">
     <div class="event-card -shadow">
-        <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
-        <h4 class="title">{{ event.title }}</h4>
-        <span>{{ event.attendees.length }} attending</span>
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <BaseIcon name="users" />
+      <span>{{ event.attendees.length }} attending</span>
     </div>
     </router-link>
 </template>
